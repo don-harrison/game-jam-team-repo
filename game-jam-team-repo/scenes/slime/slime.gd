f4b11b32d16f72ca62c30cb1802f9cb51d410820
Don Harris <donald.harrison@utah.edu>
2025-01-21 21:07:46 -0500

    quick and dirty add vector in direction of target point to simulate "spring" will add more math to simulate better. CharacterBody2D doesnt respect the physics engine so cant use dampenedspring2d. this sucks rn

--- game-jam-team-repo/scenes/slime/slime.gd
+++ game-jam-team-repo/scenes/slime/slime.gd
@@ -12,6 +12,9 @@ const MAX_HEALTH = 3
 @onready var debug_line: Node2D = $DebugLine
 @onready var health: Node = $Health
 @onready var grapple: Node = $Grapple
+var grapple_point: Vector2 = Vector2.ZERO
+const grapple_spring_constant = .5
+const grapple_damping = 1.0
 
 func _ready() -> void:
 	health.max_health = MAX_HEALTH
@@ -35,12 +38,15 @@ func _physics_process(delta: float) -> void:
 	velocity += get_gravity() * delta
 	
 	if Input.is_action_just_pressed("left_mouse_click"):
-		var relative_hit_position = raytrace(global_position, get_global_mouse_position())
-		attach_grapple(relative_hit_position)
+		var hit_position = raytrace(global_position, get_global_mouse_position())
+		attach_grapple(hit_position)
+	move_towards_grapple()
 	# Handle jump.
 	if Input.is_action_just_pressed("jump"): # Only jump while on the floor
 		velocity.y = JUMP_VELOCITY
+		grapple_point = Vector2.ZERO
 		
+	
 	# Allow for air movement control, so we don't check for on floor
 	# if we are stopped, get initial boost
 	#if Input.is_action_just_pressed("left") && velocity.x == 0:
@@ -88,7 +94,7 @@ func raytrace(origin: Vector2, end: Vector2) -> Vector2:
 		print("origin: ", origin, " end: ", end)
 
 		var space_state = get_world_2d().direct_space_state
-		var query = PhysicsRayQueryParameters2D.create(origin, end, 1)
+		var query = PhysicsRayQueryParameters2D.create(origin, end.normalized() * 200, 1)
 		query.exclude = [self, self.get_child(1)]
 		var result = space_state.intersect_ray(query)
 		var res_position = Vector2(0,0)
@@ -98,11 +104,28 @@ func raytrace(origin: Vector2, end: Vector2) -> Vector2:
 		if result.has('position'):
 			res_position = result.position
 		return res_position
-		
+	
+func move_towards_grapple():
+	#get relative position of slime to grapple anchor point
+	const speed = 50
+	if grapple_point != Vector2.ZERO:
+		var rel_position = grapple_point - position
+		velocity += (rel_position.normalized() * speed)
+	
+	
+	#var gamma: float = .5 * sqrt(4*grapple_spring_constant - grapple_damping**2)
+	#if gamma == 0: return
+	#var spring_vector: Vector2 = rel_position * (grapple_damping / (2 * gamma)) + velocity * (1/gamma)
+	#
+	#if(velocity > Vector2.ZERO):
+		#velocity += spring_vector
+	
 func attach_grapple(position: Vector2) -> void:
 	if grapple and grapple.get_child_count() > 0:
 		grapple.get_children()[0].queue_free()
-		
+	
+	grapple_point = position
+	
 	var spring: DampedSpringJoint2D = DampedSpringJoint2D.new()
 	var attach_point: Area2D = Area2D.new()
 	#var collisionShape = CollisionShape2D.new()
@@ -116,7 +139,9 @@ func attach_grapple(position: Vector2) -> void:
 	attach_point.add_child(sprite)
 	attach_point.z_index = 4
 	get_parent().add_child(attach_point)
+	#
+	#spring.node_a = self.get_path()
+	#spring.node_b = attach_point.get_path()
+	#get_parent().add_child(spring)
 	
-	spring.node_a = self.get_path()
-	spring.node_b = attach_point.get_path()
 	
