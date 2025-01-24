794d0e39df8bf859de5b34f556c60abd61966b03
Don Harris <donald.harrison@utah.edu>
2025-01-23 19:11:27 -0500

    Adding some animations and configured. animation transitions are a little slow.

--- game-jam-team-repo/slime_animation_tree.gd
+++ game-jam-team-repo/slime_animation_tree.gd
@@ -6,15 +6,29 @@ extends AnimationTree
 func _ready() -> void:
 	pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	update_animation_parameters(delta)
 
 func update_animation_parameters(delta: float):
-	if slime.velocity < Vector2(1, 15) && slime.velocity > Vector2(-1, -15):
-		self["parameters/conditions/idle"] = true
-		self.active = true
-	else:
+	self.active = true
+	
+	self["parameters/Jump/blend_position"] = Vector2(slime.velocity.x,slime.velocity.y)
+	self["parameters/Fall/blend_position"] = Vector2(slime.velocity.x,slime.velocity.y)
+	
+	if slime.velocity.y < 15:
+		print("jump")
+		self["parameters/conditions/jump"] = true
+		self["parameters/conditions/fall"] = false
 		self["parameters/conditions/idle"] = false
-		self.active = false
+		
+	if slime.velocity.y > -15:
+		print("fall")
+		self["parameters/conditions/jump"] = false
+		self["parameters/conditions/fall"] = true
+		self["parameters/conditions/idle"] = false
+		
+	if slime.velocity < Vector2(1, 15) && slime.velocity > Vector2(-1, -15):
+		self["parameters/conditions/jump"] = false
+		self["parameters/conditions/fall"] = false
+		self["parameters/conditions/idle"] = true
