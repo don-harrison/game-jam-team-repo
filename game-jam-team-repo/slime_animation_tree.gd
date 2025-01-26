297a635d7aa822929bbbde9ab2aa8fab2e21177d
Don Harris <donald.harrison@utah.edu>
2025-01-25 21:35:53 -0500

    added more animations, kinda figuring out animation tree. Definitely very messy and confusing scripts for managing animations. kinda working rn, biggest issue is animations arent very seamless between transitions

--- game-jam-team-repo/slime_animation_tree.gd
+++ game-jam-team-repo/slime_animation_tree.gd
@@ -1,27 +1,42 @@
 extends AnimationTree
 
 @onready var slime: Slime = $".."
-
+var last_bounce_normal: Vector2
+var state
+enum {
+	DEFAULT,
+	WALL_BOUNCE,
+	FLOOR_BOUNCE,
+	IDLE
+}
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SignalManager.slime_bounce.connect(slime_bounce)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	state = DEFAULT
 	update_animation_parameters(delta)
 
 func slime_bounce(normal: Vector2):
+	last_bounce_normal = normal
+	print(last_bounce_normal)
 	#floor bounce
 	if(normal.y == -1):	
-		pass
-		#var state_machine:AnimationNodeStateMachine = self["tree_root"]
-		#state_machine.start
+		#if slime.velocity.y > 15 && slime.velocity.y < -15:
+		state = FLOOR_BOUNCE
 	#wall bounce
-	else:	
-		pass
+	if(normal.x == -1):
+		print(last_bounce_normal)
+		self["parameters/Wall_Bounce/blend_position"] = last_bounce_normal
+		state = WALL_BOUNCE
+
+	if(normal.x == 1):
+		print(last_bounce_normal)
+		self["parameters/Wall_Bounce/blend_position"] = last_bounce_normal
+		state = WALL_BOUNCE
 
 func update_animation_parameters(delta: float):
-	#print(slime.velocity)
 	self.active = true
 	
 	self["parameters/Jump/blend_position"] = Vector2(slime.velocity.x,slime.velocity.y)
